0863a89abe1a36ebf68cea01807a7875120457dd
tharunishkumar <118017668+tharunishkumar@users.noreply.github.com>
2024-07-20 18:45:49 +0530

    Add files via upload

---
+++ summarize-and-chat-with-youtube-video/app.py
@@ -0,0 +1,120 @@
+import os
+from queue import Queue, Empty
+from threading import Thread
+from typing import Any
+import argparse
+
+import gradio as gr
+import openai
+from dotenv import load_dotenv, find_dotenv
+from langchain.callbacks.base import BaseCallbackHandler
+
+from footer import DESCRIPTION, loading_message
+from model import ConversationalRetrievalChain
+from video_summary import Summary
+
+# Load OpenAI API key from environment variables
+openai.api_key = load_dotenv(find_dotenv())
+
+# Save credentials for gradio app
+secret_key = os.getenv('OPENAI_API_KEY')
+
+
+def main():
+    # Initialize a queue for communication between threads
+    q: Queue = Queue()
+    job_done = object()
+
+    class QueueCallback(BaseCallbackHandler):
+        """Callback handler for streaming LLM (LangChain) responses to a queue."""
+
+        def __init__(self, q: Queue):
+            self.q = q
+
+        def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+            """Callback method to add new token to the queue."""
+            self.q.put(token)
+
+        def on_llm_end(self, *args, **kwargs: Any) -> bool:
+            """Callback method called when LLM interaction ends."""
+            return self.q.empty()
+
+    # Define a list of callbacks to be used by the LLM (LangChain)
+    callbacks = [QueueCallback(q)]
+
+    def answer(question: str) -> None:
+        """Function to predict response to a given question."""
+
+        def task():
+            qa_chain = ConversationalRetrievalChain(callbacks=callbacks,
+                                                    url=URL
+                                                    ).create_chain()
+            response = qa_chain({"query": question})
+            q.put(job_done)
+
+        t = Thread(target=task)
+        t.start()
+
+    # Create a gradio block
+    blocker = gr.Blocks(
+        theme=gr.themes.Default(primary_hue=gr.themes.colors.green, secondary_hue=gr.themes.colors.green),
+        title='YouTube Chatbot',
+    )
+
+    # Define the Gradio interface
+    with blocker as demo:
+        gr.Markdown(DESCRIPTION)
+
+        with gr.Tab("QnA"):
+
+            chatbot = gr.Chatbot()
+            message = gr.Textbox()
+            clear = gr.Button("Clear")
+
+            def user(user_message: str, history: list) -> tuple:
+                """Function to process user input."""
+                return "", history + [[user_message, None]]
+
+            def bot(history: list) -> Any:
+                """Function to handle bot responses."""
+                question = history[-1][0]
+                print("Question: ", question)
+                history[-1][1] = ""
+                answer(question=question)
+                while True:
+                    try:
+                        next_token = q.get(True)
+                        if next_token is job_done:
+                            break
+                        history[-1][1] += next_token
+                        yield history
+                    except Empty:
+                        continue
+
+            # Define Gradio components and interactions
+            message.submit(user, [message, chatbot], [message, chatbot], queue=False).then(
+                bot, chatbot, chatbot
+            )
+            clear.click(lambda: None, None, chatbot, queue=False)
+
+        with gr.Tab("Summary - Paragraph"):
+            summary = Summary(URL).get_refine_summary()
+            gr.Textbox(summary)
+
+        with gr.Tab("Summary - Key Points"):
+            summary = Summary(URL).get_map_reduce_summary()
+            gr.Textbox(summary)
+
+    # Launch the Gradio interface
+    demo.queue()
+    demo.launch()
+
+
+if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Process URL for the QA model.')
+    parser.add_argument('--url', type=str, help='URL for the QA model')
+    args = parser.parse_args()
+
+    URL: str = args.url
+    main()
---
+++ summarize-and-chat-with-youtube-video/footer.py
@@ -0,0 +1,36 @@
+DESCRIPTION = """
+
+<h1> <center> With LangChain 🦜🔗 & Gradio, chat with a YouTube Video.</center> </h1>
+
+<h4>
+You have the following tabs:
+</h4>
+
+<ol>
+  <li> </h4>QnA</h4>: To perform a conversation</li>
+  <li> </h4>Summary - Paragraph</h4> : To view the summary as a whole detailed paragraph using Refine method</li>
+  <li> </h4>Summary - Key Points</h4> : To view the summary as main points using Map Reduce method</li>
+</ol> 
+"""
+
+loading_message = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loading...</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding-top: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h2>Loading... Please wait.</h2>
+</body>
+</html>
+"""
